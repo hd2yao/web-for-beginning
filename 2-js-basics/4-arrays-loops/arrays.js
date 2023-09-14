e67597830212037aa665503d8844af119feac25a
hd2yao <1054290355@qq.com>
2023-09-14 11:16:29 +0800

    create arrays

---
+++ 2-js-basics/4-arrays-loops/arrays.js
@@ -0,0 +1,8 @@
+// 数组 arrays
+let myArrays = [];
+
+let iceCreamFlavors = ["Chocolate", "Strawberry", "Vanilla", "Pistachio", "Rocky Road"];
+iceCreamFlavors[2]; // "Vanilla"
+iceCreamFlavors[4] = "Butter Pecan";
+iceCreamFlavors[5] = "Cookie Dough"; // 插入新值
+iceCreamFlavors.push("") // 使用数组方法添加新值
