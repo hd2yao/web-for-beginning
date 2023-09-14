bf139ce20084d7eb517cff9ded04c176ac6df12a
hd2yao <1054290355@qq.com>
2023-09-14 11:22:27 +0800

    loops

--- 2-js-basics/4-arrays-loops/arrays.js
+++ 2-js-basics/4-arrays-loops/arrays.js
@@ -6,3 +6,22 @@ iceCreamFlavors[2]; // "Vanilla"
 iceCreamFlavors[4] = "Butter Pecan";
 iceCreamFlavors[5] = "Cookie Dough"; // 插入新值
 iceCreamFlavors.push("") // 使用数组方法添加新值
+iceCreamFlavors.length; // 6 获取数组元素数量
+
+
+// loops
+// for
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+// while
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+for (let i = 0; i < iceCreamFlavors.length; i++) {
+    console.log(iceCreamFlavors[i]);
+}
