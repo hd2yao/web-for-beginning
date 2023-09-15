90410ae49aae11f3735b46d23cae9a2921748a9c
hd2yao <1054290355@qq.com>
2023-09-15 14:46:20 +0800

    create form fields and results divs variable

--- 5-browser-extension/start/src/index.js
+++ 5-browser-extension/start/src/index.js
@@ -1,6 +1,16 @@
 //1
 // form fields
+const form = document.querySelector('.form-data');
+const region = document.querySelector('.region-name');
+const apiKey = document.querySelector('.api-key');
 // results divs
+const errors = document.querySelector('.errors');
+const loading = document.querySelector('.loading');
+const results = document.querySelector('.result-container');
+const usage = document.querySelector('.carbon-usage');
+const fossilFuel = document.querySelector('.fossil-fuel');
+const myRegion = document.querySelector('.my-region');
+const clearBtn = document.querySelector('.clear-btn');
 
 //6
 //call the API
