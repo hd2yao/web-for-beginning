e44b4c8e3fbfd2a99ade9e173916632ff75c94fd
hd2yao <1054290355@qq.com>
2023-09-15 15:58:54 +0800

    call the API

--- 5-browser-extension/start/src/index.js
+++ 5-browser-extension/start/src/index.js
@@ -1,3 +1,5 @@
+import axios from '../node_modules/axios'
+
 //1
 // form fields
 const form = document.querySelector('.form-data');
@@ -14,6 +16,39 @@ const clearBtn = document.querySelector('.clear-btn');
 
 //6
 //call the API
+async function displayCarbonUsage(apiKey, region) {
+    try {
+        await axios
+        .get('https://api.co2signal.com/v1/latest', {
+            params: {
+                countryCode: region,
+            },
+            headers: {
+                'auth-token': apiKey,
+            },
+        })
+        .then((response) => {
+            let CO2 = Math.floor(response.data.data.carbonIntensity);
+
+            // calculateColor(CO2);
+
+            loading.style.display = 'none';
+            form.style.display = 'none';
+            myRegion.textContent = region;
+            usage.textContent = 
+                Math.round(response.data.data.carbonIntensity) + ' grams (grams C02 emitted per kilowatt hour)';
+            fossilFuel.textContent = 
+                response.data.data.fossilFuelPercentage.toFixed(2) +
+                '% (percentage of fossil fuels used to generate electricity)';
+            results.style.display = 'block';
+        });
+    } catch (error) {
+        console.log(error);
+        loading.style.display = 'none';
+        results.style.display = 'none';
+        errors.textContent = 'Sorry, we have no data for the region you have requested.';
+    }
+}
 
 //5
 //set up user's api key and region
