990fc2920f28bffdc514bbe45f42d466a156d9a1
hd2yao <1054290355@qq.com>
2023-09-15 13:15:44 +0800

    add typing logic

--- 4-typing-game/script.js
+++ 4-typing-game/script.js
@@ -6,7 +6,7 @@ const quotes = [
     'What one can invent another can discover.',
     'Nothing clears up a case so much as stating it to another person.',
     'Education never ends, Watson. It is a series of lessons, with the greatest for the last.',
-]
+];
 
 // 储存单词列表以及当前要输入的单词索引
 let words = [];
@@ -29,7 +29,7 @@ document.getElementById('start').addEventListener('click', () => {
 
     // 更新使用者界面
     // 建立 span 元素的矩阵，设定 class 使用
-    const spanWords = words.map(function(word) { return `<span>${word}</span>`});
+    const spanWords = words.map(function(word) { return `<span>${word} </span>`});
     // 转换成字符串并以 innerHTML 显示引文
     quoteElement.innerHTML = spanWords.join('');
     // 标记第一个单词
@@ -46,4 +46,38 @@ document.getElementById('start').addEventListener('click', () => {
 
     // 开始计时器
     startTime = new Date().getTime();
+});
+
+typedValueElement.addEventListener('input', () => {
+    // 取得当前的单词
+    const currentWord = words[wordIndex];
+    // 取得当前输入的数值
+    const typedValue = typedValueElement.value;
+
+    if (typedValue == currentWord && wordIndex == words.length - 1) {
+        // 句子最末端
+        // 显示成功
+        const elapsedTime = new Date().getTime() - startTime;
+        const message = `CONGRATULATIONS! You finished in ${elapsedTime / 1000} seconds.`;
+        messageElement.innerText = message;
+    } else if (typedValue.endsWith(' ') && typedValue.trim() === currentWord) {
+        // 单词最末端
+        // 清除输入的数值，准备给新的单词使用
+        typedValueElement.value = '';
+        // 移动到下一个单词
+        wordIndex++;
+        // 重设所有引文子元素的 class 名称
+        for (const wordElement of quoteElement.childNodes) {
+            wordElement.className = '';
+        }
+        // 标记新单词
+        quoteElement.childNodes[wordIndex].className = 'highlight';
+    } else if (currentWord.startsWith(typedValue)) {
+        // 单词当前输入正确
+        // 标记下一个单词
+        typedValueElement.className = '';
+    } else {
+        // 单词输入错误
+        typedValueElement.className = 'error';
+    }
 });
